611e7c8b04a6636da6aefd6f411d071920cf9bd3
Your Name <you@example.com>
2025-10-09 23:16:02 -0300

    feat: add designer

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Image from 'next/image';
+import styles from './Header.module.css';
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <div className={styles.logoContainer}>
+        <Image
+          src="/logo.png"
+          alt="CareMind Logo"
+          width={150}
+          height={40}
+          priority
+        />
+      </div>
+    </header>
+  );
+}
